10aba30c856480eadd7312984f9ac71c582509e5
Nitin Misra <nitin.misra23@gmail.com>
2018-04-26 21:45:08 +0530

    Move project_id module attribute to a private function

--- lib/messaging.ex
+++ lib/messaging.ex
@@ -3,10 +3,6 @@ defmodule FirebaseAdminEx.Messaging do
   alias FirebaseAdminEx.Messaging.Message
 
   @fcm_endpoint "https://fcm.googleapis.com/v1"
-  @project_id Application.get_env(:firebase_admin_ex, :project_id)
-
-  # Define API endpoints
-  @send_url "#{@fcm_endpoint}/projects/#{@project_id}/messages:send"
 
   # Public API
 
@@ -18,7 +14,7 @@ defmodule FirebaseAdminEx.Messaging do
   @spec send(String.t(), struct()) :: tuple()
   def send(oauth_token, %Message{} = message) do
     with {:ok, message} <- Message.validate(message),
-         {:ok, response} <- Request.post(@send_url, %{message: message}, auth_header(oauth_token)),
+         {:ok, response} <- Request.post(send_url(), %{message: message}, auth_header(oauth_token)),
          {:ok, body} <- Response.parse(response) do
       {:ok, body}
     else
@@ -28,6 +24,13 @@ defmodule FirebaseAdminEx.Messaging do
   end
 
   # Private API
+  defp project_id do
+    Application.get_env(:firebase_admin_ex, :project_id)
+  end
+
+  defp send_url do
+    "#{@fcm_endpoint}/projects/#{project_id()}/messages:send"
+  end
 
   defp auth_header(oauth_token) do
     %{"Authorization" => "Bearer #{oauth_token}"}
