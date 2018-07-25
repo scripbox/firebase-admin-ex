69462946e517757c54350be9b96daec0960c58fd
Nitin Misra <nitin.misra23@gmail.com>
2018-07-25 23:18:06 +0530

    Fix HTTPoison.Base warnings

--- lib/request.ex
+++ lib/request.ex
@@ -7,15 +7,15 @@ defmodule FirebaseAdminEx.Request do
     %{"Content-Type" => "application/json"}
   end
 
-  defp process_request_body(%{message: %Message{} = _message} = body) do
+  def process_request_body(%{message: %Message{} = _message} = body) do
     body
     |> Poison.encode!()
   end
 
-  defp process_request_body(body), do: body
+  def process_request_body(body), do: body
 
   # Override the base headers with any passed in.
-  defp process_request_headers(request_headers) do
+  def process_request_headers(request_headers) do
     headers =
       request_headers
       |> Enum.into(%{})
@@ -26,7 +26,7 @@ defmodule FirebaseAdminEx.Request do
 
   # :timeout - timeout to establish a connection, in milliseconds.
   # :recv_timeout - timeout used when receiving a connection.
-  defp process_request_options(_options) do
+  def process_request_options(_options) do
     [timeout: 5000, recv_timeout: 2000]
   end
 end
