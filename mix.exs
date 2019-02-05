8a9c87149473d6f2c40a57bb002d865eafc5c263
Nitin Misra <nitin.misra23@gmail.com>
2019-02-05 15:40:05 +0530

    Add :ex_doc package to generate documentation

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule FirebaseAdminEx.MixProject do
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      organization: "scripbox",
+      description: description(),
+      package: package(),
       source_url: "https://github.com/scripbox/firebase-admin-ex",
       homepage_url: "https://github.com/scripbox/firebase-admin-ex"
     ]
@@ -27,7 +28,20 @@ defmodule FirebaseAdminEx.MixProject do
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1.0"},
       {:mock, "~> 0.3.0", only: :test},
-      {:goth, "~> 0.11.0"}
+      {:goth, "~> 0.11.0"},
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "The Firebase Admin Elixir SDK"
+  end
+
+  defp package do
+    [
+      licenses: ["Apache 2.0"],
+      organization: "scripbox",
+      links: %{"GitHub" => "https://github.com/scripbox/firebase-admin-ex"}
     ]
   end
 end
