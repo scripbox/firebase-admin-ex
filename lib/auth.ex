e809b57fe0781ec8bfecb4c3118a9b84c5ff54b9
martinjhm <martin.hernandez@resuelvetudeuda.com>
2019-03-22 17:12:12 -0500

    Agrega soporte para generar action link

--- lib/auth.ex
+++ lib/auth.ex
@@ -2,8 +2,22 @@ defmodule FirebaseAdminEx.Auth do
   alias FirebaseAdminEx.{Request, Response, Errors}
 
   @auth_endpoint "https://www.googleapis.com/identitytoolkit/v3/relyingparty/"
+  @auth_endpoint_email "https://identitytoolkit.googleapis.com/v1/projects/"
   @auth_scope "https://www.googleapis.com/auth/cloud-platform"
 
+  @valids_action_code_settings [
+    "requestType",
+    "email",
+    "returnOobLink",
+    "continueUrl",
+    "canHandleCodeInApp",
+    "dynamicLinkDomain",
+    "androidPackageName",
+    "androidMinimumVersion",
+    "androidInstallApp",
+    "iOSBundleId"
+  ]
+  
   @doc """
   Get a user's info by UID
   """
@@ -46,6 +60,43 @@ defmodule FirebaseAdminEx.Auth do
   @spec create_email_password_user(map, String.t() | nil) :: tuple()
   def create_email_password_user(%{"email" => email, "password" => password}, client_email \\ nil),
     do: do_request("signupNewUser", %{:email => email, :password => password, :returnSecureToken => true}, client_email)
+
+
+  @doc """
+  Generates the email action link for sign-in flows, using the action code settings provided
+  """
+  @spec generate_sign_in_with_email_link(String.t() ,map, String.t() | nil, String.t()) :: tuple()
+  def generate_sign_in_with_email_link(email, action_code_settings \\ nil, client_email \\ nil, project_id) do
+    initial_settings = %{
+      "requestType" => "EMAIL_SIGNIN",
+      "email" => email,
+      "returnOobLink" => true
+    }
+    payload = 
+      unless action_code_settings == nil do
+        action_code_settings
+        |> Map.take(@valids_action_code_settings)
+        |> Map.merge(initial_settings, fn _k, _v1, v2 -> v2 end)
+      else ->
+        initial_settings
+      end
+    do_request("accounts:sendOobCode", payload, client_email, project_id, :accounts)
+  end
+   
+  defp do_request(url_suffix, payload, client_email, project_id, :accounts) do
+    with {:ok, response} <-
+           Request.request(
+             :post,
+             "#{@auth_endpoint_email}#{project_id}/#{url_suffix}",
+             payload,
+             auth_header(client_email)
+           ),
+         {:ok, body} <- Response.parse(response) do
+      {:ok, body}
+    else
+      {:error, error} -> raise Errors.ApiError, Kernel.inspect(error)
+    end
+  end
 
   defp do_request(url_suffix, payload, client_email) do
     with {:ok, response} <-
